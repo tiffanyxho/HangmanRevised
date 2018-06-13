2e8cfa6a8bc11ece162700bfefec36251d3181d1
TiffanyH <tiffanyxho@gmail.com>
2018-06-13 11:24:13 -0700

    testing - got a red rect moving right and left inside border and green rect moving up and down inside border

--- HangmanRevised.js
+++ HangmanRevised.js
@@ -6,26 +6,48 @@ var canvas = document.getElementById('canvas');
 var worker;
 
 function main(){
-    //countdown();
+    var hit_right = false, hit_bottom = false;
     var context = canvas.getContext('2d');
     context.beginPath();
     context.arc(canvas.width/2,0,10,0,2*Math.PI);
     context.stroke();
 
-    /*window.requestAnimationFrame(function loop(){
-        x_pos += 1;
-        y_pos += .5;
-
+    window.requestAnimationFrame(function loop(){
+        if (x_pos >= 0 && x_pos < canvas.width - 100 & !hit_right){
+            x_pos += 1;
+        }
+        if (x_pos === canvas.width - 100){
+            hit_right = true;
+        }
+        if (hit_right && x_pos !== 0){
+            x_pos -= 1;
+        }
+        if (x_pos === 0){
+            hit_right = false;
+        }
+        if (y_pos >= 0 && y_pos < canvas.height - 100 & !hit_bottom){
+            y_pos += 1;
+        }
+        if (y_pos === canvas.height - 100){
+            hit_bottom = true;
+        }
+        if (hit_bottom && y_pos !== 0){
+            y_pos -= 1;
+        }
+        if (y_pos === 0){
+            hit_bottom = false;
+        }
+        
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         context.fillStyle = 'red';
-        context.fillRect(x_pos, 0, 100, 100);
+        context.fillRect(x_pos, 10, 100, 100);
 
         context.fillStyle = 'green';
-        context.fillRect(0, y_pos, 100, 100);
+        context.fillRect(10, y_pos, 100, 100);
 
         window.requestAnimationFrame(loop);
-    });*/
+    });
 
     document.addEventListener('mousedown',  mouseDown);
 }
