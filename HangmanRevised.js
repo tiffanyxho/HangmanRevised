3199e2c15c321e3d9296fcb84d3b8897e86a828a
TiffanyH <tiffanyxho@gmail.com>
2018-06-13 13:52:30 -0700

    added new JS file for learning canvas and testing, deleted testing code from HangmanRevised.js file

--- HangmanRevised.js
+++ HangmanRevised.js
@@ -10,45 +10,9 @@ function main(){
     context.arc(canvas.width/2,0,10,0,2*Math.PI);
     context.stroke();
 
-    /*
-    window.requestAnimationFrame(function loop(){
-        if (x_pos >= 0 && x_pos < canvas.width - 100 & !hit_right){
-            x_pos += 1;
-        }
-        if (x_pos === canvas.width - 100){
-            hit_right = true;
-        }
-        if (hit_right && x_pos !== 0){
-            x_pos -= 1;
-        }
-        if (x_pos === 0){
-            hit_right = false;
-        }
-        if (y_pos >= 0 && y_pos < canvas.height - 100 & !hit_bottom){
-            y_pos += 1;
-        }
-        if (y_pos === canvas.height - 100){
-            hit_bottom = true;
-        }
-        if (hit_bottom && y_pos !== 0){
-            y_pos -= 1;
-        }
-        if (y_pos === 0){
-            hit_bottom = false;
-        }
-        
-        context.clearRect(0, 0, canvas.width, canvas.height);
-
-        context.fillStyle = 'red';
-        context.fillRect(x_pos, 10, 100, 100);
-
-        context.fillStyle = 'green';
-        context.fillRect(10, y_pos, 100, 100);
-
-        window.requestAnimationFrame(loop);
-    });*/
-
-    document.addEventListener('mousedown',  mouseDown);
+    context.font = "20px Georgia";
+    context.strokeText("hello", 10, 100);
+    console.log(document.getElementById("countdown"));
 }
 
 // TODO: Web Worker for a timer - does not work, gives an error in Chrome dev. tools
@@ -62,12 +26,5 @@ window.onload = function countdown(){
         };
     }else{
         document.getElementById("countdown").innerHTML = "not supported";
-    }
-}
-
-
-function mouseDown(event){
-    if (event.button === 0){
-        x_pos += 10;
     }
 }
